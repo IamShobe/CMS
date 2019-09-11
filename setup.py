3c260587e5c5a03fb1edc6b49887f9ffad2052da
Elran Shefer <elran777@gmail.com>
2019-09-11 22:38:35 +0300

    added requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Car Project',
+    name='CMS',
     version='0.1.0',
     url='https://github.com/IamShobe/car.git',
     author='Elran Shefer',
     author_email='elran777@gmail.com',
-    description='{description}',
+    description='Car Multimedia Software',
     packages=find_packages("src"),
     install_requires=[],
     package_dir={"": "src"},
